8c1127ad2e5d3697185335b9b28ed1f8f551c892
pithreeone <pithreeone@gmail.com>
2024-01-03 11:49:02 +0800

    Feat: filt the result

---
+++ vehicle_detection/filter.py
@@ -0,0 +1,7 @@
+import json
+
+file_path = '/home/pithreeone/SDC-Repo/2023_final/vehicle_detection/output_comp_3.json'
+with open(file_path, 'r') as json_file:
+    data_raw = json.load(json_file)
+
+    
